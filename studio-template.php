1c3fd449b4315fc260a23719ab2a6ef48595ada7
timshoesmith <info@tjshoesmith.co.uk>
2022-10-06 11:51:52 +0100

    studio page added

---
+++ studio-template.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Template Name: Studio
+*/
+?>
+
+
+<?php get_header(); ?>
+<div class="container">
+            <h1><?php the_title() ?></h1>
+            <div class="full-width-section display-flex">
+                 <!-- Test Box -->
+                <div class="text-box half-width-section">
+                    <h3 class="text-title">Microphone</h3>
+                    <p class="paragraph-text">Many years ago I bought an Audio Technica Cardioid microphone. It lay around for many years. When I built the booth I found it. It served me well for nearly a year. It is a good, robust piece of kit but low end. After some success I bought the Sontronics STC-3X. This added a great deal to the sound and I have been using it ever since.</p>
+                </div>
+                 <!-- Video container -->
+                 <div class="half-width-section">                                                
+                    <img src="<?php echo get_theme_file_uri('/img/studio/sontronics.jpg') ?>" alt="microphone">                                  
+                </div>
+            </div>
+            <div class="full-width-section display-flex display-direction-reverse">
+                 <!-- Test Box -->
+                <div class="text-box half-width-section">
+                    <h3 class="text-title">Hardware</h3>
+                    <p class="paragraph-text">I am running both an intel i7 docked laptop and an iMac. Most administration is done on the laptop and occasional editing work when I’m away from the office. However, the iMac does the heavy lifting. It is powerful enough to run the Adobe suite and it is so reliable.</p>
+                </div>
+                 <!-- Video container -->
+                 <div class="half-width-section">
+                 <img src="<?php echo get_theme_file_uri('/img/studio/apple-i7.jpg') ?>" alt="hardware">                                  
+                </div>
+            </div>
+            <div class="full-width-section display-flex">
+                 <!-- Test Box -->
+                <div class="text-box half-width-section">
+                    <h3 class="text-title">Software</h3>
+                    <p class="paragraph-text">I have always edited using Adobe Audition. However, I have added a few plugins along the way. Isotopes de-clicker is probably the best there is and Waves noise reduction is simple and effective. The compression in Adobe is good and practical.</p>
+                </div>
+                 <!-- Video container -->
+                 <div class="half-width-section">
+                 <img src="<?php echo get_theme_file_uri('/img/studio/au-rx-adobe.jpg') ?>" alt="pre-amp">                                  
+                </div>
+            </div>
+            <div class="full-width-section display-flex display-direction-reverse">
+                 <!-- Test Box -->
+                <div class="text-box half-width-section">
+                    <h3 class="text-title">Preamp</h3>
+                    <p class="paragraph-text">As we all know you can’t use a condenser without a preamp and Focusrite offers affordable and effective devices. For a while I used the Focusrite iTrack Dock. I still do when away from the booth. In the studio we have the Focusrite Scarlett 2i2.</p>
+                </div>
+                 <!-- Video container -->
+                 <div class="half-width-section">
+                 <img src="<?php echo get_theme_file_uri('/img/studio/scarlett.jpg') ?>" alt="pre-amp">                                  
+                </div>
+            </div>
+            <div class="full-width-section display-flex">
+                 <!-- Test Box -->
+                <div class="text-box half-width-section">
+                    <h3 class="text-title">Communication</h3>
+                    <p class="paragraph-text">I use Skype, Zoom and good old fashioned telephone. So, if you want to direct a session it is easy. Files are loaded into Dropbox or Google Drive in real time, so you get full quality files as we work.</p>
+                </div>
+                 <!-- Text Box -->
+                 <div class="text-box half-width-section">
+                    <h3 class="text-title">Delivery</h3>
+                    <p class="paragraph-text">Delivery includes, Email, CD, USB Stick, Dropbox, Google Drive.</p>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+<?php get_footer(); ?>
